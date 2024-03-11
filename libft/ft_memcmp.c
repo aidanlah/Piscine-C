c96625df8eb807d00de33200fc0ff917eaa3e425
Aidan Leong <aileong@u91z04s02.42kl.edu.my>
2024-03-11 19:56:39 +0800

    yuh

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -21,11 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	c1 = (unsigned char *)s1;
 	c2 = (unsigned char *)s2;
-	while (i < n)
+	if (n == 0)
+		return (0);
+	while (i < (n - 1) && *(c1 + i) && *(c1 + i) == *(c2 + i))
 	{
-		if (*(c1 + i) != '\0' && *(c1 + i) == *(c2 + i) && n > 0)
-			return (*(c1 + i) - *(c2 + i));
-		i ++;
+		i++;
 	}
-	return (0);
+	return (*(c1 + i) - *(c2 + i));
 }
---
+++ libft/main.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("%d", ft_isdigit('3'));
+}
